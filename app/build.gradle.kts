ebafba44354eeeebc033d11901ff255fa8f753e0
Andres Cubillos <andrestj1996@gmail.com>
2023-11-23 12:43:36 -0300

    primeras configuraciones

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.calendariouniversitario"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.calendariouniversitario"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
